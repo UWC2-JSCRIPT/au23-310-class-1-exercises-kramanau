ac9c0768899f823e4640fcadc9846554284fef8f
Karolis Ramanauskas <kramanauskas112@gmail.com>
2023-10-16 20:02:27 -0700

    finished problem 1,2,3,4

--- Class 1 Exercises.js
+++ Class 1 Exercises.js
@@ -5,19 +5,42 @@
 // 1. Pagliacci charges $16.99 for a 13” pizza and $19.99 for a 17” pizza.
 // What is the area for each of these pizzas?
 // (radius would be the listed size - i.e. 13" - divided by 2)
+const SMALL_PIZZA_DIAMETER = 13;
+const LARGE_PIZZA_DIAMETER = 17;
 
+const smallPizzaArea = Math.PI * ((SMALL_PIZZA_DIAMETER / 2) ** 2);
+const largePizzaArea = Math.PI * ((LARGE_PIZZA_DIAMETER / 2) ** 2);
+
+console.log('Small pizza area: ', smallPizzaArea);
+console.log('Large pizza area: ', largePizzaArea);
 
 // 2. What is the cost per square inch of each pizza?
+const SMALL_PIZZA_PRICE = 16.99;
+const LARGE_PIZZA_PRICE = 19.99;
 
+const smallPizzaCostPerSquareInch = smallPizzaArea / SMALL_PIZZA_PRICE;
+const largePizzaCostPerSquareInch = largePizzaArea / LARGE_PIZZA_PRICE;
+
+console.log('Small pizza cost per sq inch: ', smallPizzaCostPerSquareInch);
+console.log('Large pizza cost per sq inch: ', largePizzaCostPerSquareInch);
 
 // 3. Using the Math object, put together a code snippet
 // that allows you to draw a random card with a value
 // between 1 and 13 (assume ace is 1, jack is 11…)
-
+function drawCard() {
+    const card = Math.floor(Math.random() * 13) + 1;
+    return card;
+}
 
 // 4. Draw 3 cards and use Math to determine the highest
 // card
+const cardOne = drawCard();
+const cardTwo = drawCard();
+const cardThree = drawCard();
 
+const highestCard = Math.max(cardOne, cardTwo, cardThree);
+
+console.log('Highest card: ', highestCard);
 
 /**
  * ADDRESS LINE
